b9698e1698116c919e94865d28e2d151aaf2a1d1
ZAN DoYe <357626+kandu@users.noreply.github.com>
2014-11-06 18:16:31 +0800

    better error report and support for alternative attribute to indicate current monad module

--- ppx_ok_monad.ml
+++ ppx_ok_monad.ml
@@ -4,9 +4,15 @@ open Parsetree
 open Ast_helper
 open Ast_mapper
 
-exception Error of Location.t
+open Location
 
-let ident_bind= Exp.ident {txt= Longident.parse "bind"; loc= Location.none}
+let ident_bind moduleName=
+  let bind=
+    (match moduleName with
+    | "" -> ""
+    | s -> s ^ ".")
+    ^ "bind"
+  in Exp.ident {txt= Longident.parse bind; loc= Location.none}
 
 let rec cps_sequence mapper expr=
   match expr with
@@ -15,10 +21,26 @@ let rec cps_sequence mapper expr=
       pexp_loc;
       pexp_attributes;
     } ->
-      Exp.(apply ident_bind [
-        ("", expr1);
-        ("", fun_ "" None (Pat.any ()) (cps_sequence mapper expr2));
-        ])
+      let ident_bind=
+        match pexp_attributes with
+        | [] -> ident_bind ""
+        | [(loc,_)]-> ident_bind loc.txt
+        | _::(loc,_)::_->
+          raise (Error (error ~loc:loc.loc "too many attributes" ~if_highlight:loc.txt))
+      in
+      let rec do_cps_sequence mapper expr=
+        (match expr with
+        | {
+            pexp_desc= Pexp_sequence (expr1, expr2);
+            pexp_loc;
+            pexp_attributes;
+          } ->
+            Exp.(apply ident_bind [
+              ("", expr1);
+              ("", fun_ "" None (Pat.any ()) (do_cps_sequence mapper expr2));
+              ])
+        | _ -> default_mapper.expr mapper expr)
+      in do_cps_sequence mapper expr
   | _ -> default_mapper.expr mapper expr
 
 let cps_let mapper expr=
@@ -28,11 +50,19 @@ let cps_let mapper expr=
       pexp_loc;
       pexp_attributes;
     } ->
-      Exp.(apply ident_bind [
-        ("", binding.pvb_expr);
-        ("", fun_ "" None binding.pvb_pat expr);
-        ])
-  
+      (match pexp_attributes with
+      | []->
+        Exp.(apply (ident_bind "") [
+          ("", binding.pvb_expr);
+          ("", fun_ "" None binding.pvb_pat expr);
+          ])
+      | [(loc,_)]->
+        Exp.(apply (ident_bind loc.txt) [
+          ("", binding.pvb_expr);
+          ("", fun_ "" None binding.pvb_pat expr);
+          ])
+      | _::(loc,_)::_->
+        raise (Error (error ~loc:loc.loc "too many attributes" ~if_highlight:loc.txt)))
   | _ -> default_mapper.expr mapper expr
 
 let cps_mapper argv=
@@ -45,7 +75,7 @@ let cps_mapper argv=
           { (expr |> cps_sequence mapper |> cps_let mapper)
             with pexp_loc= expr.pexp_loc
           }
-        | _ -> raise (Error loc))
+        | _ -> raise (Error (error ~loc:loc "unknown error")))
       | _ -> default_mapper.expr mapper expr
   }
 
